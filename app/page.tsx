0b0bb6f1e62ed685ab407ff04b5b22fe85759891
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-07-10 22:30:57 -0700

    adding assets and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
